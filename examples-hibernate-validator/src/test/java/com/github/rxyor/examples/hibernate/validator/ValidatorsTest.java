c516a1e59c90deebf65554e6cbbeddb6eabc9f72
qianmu.ly.千霂 <rxyor@outlook.com>
2021-04-07 13:20:13 +0800

    校验工具

---
+++ examples-hibernate-validator/src/test/java/com/github/rxyor/examples/hibernate/validator/ValidatorsTest.java
@@ -0,0 +1,30 @@
+package com.github.rxyor.examples.hibernate.validator;
+
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Range;
+import org.junit.jupiter.api.Test;
+
+/**
+ *<p>
+ *
+ *</p>
+ *
+ * @author liuyang
+ * @since 2021-04-07 v1.0
+ */
+class ValidatorsTest {
+
+    @Test
+    void validate() {
+        Validators.FailNotFastValidator.validate(new Bean());
+    }
+
+    public static class Bean {
+        @NotBlank(message = "姓名不能为空")
+        private String name;
+
+        @Range(min = 0L, max = 150L, message = "年龄应该在{min}~{max}之间")
+        private Integer age;
+    }
+}
